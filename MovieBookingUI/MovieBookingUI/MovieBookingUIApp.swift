569f1c582023a456e80ca986f542e0cc00e21631
codersinfo <coders.infoapp@gmail.com>
2023-12-19 17:18:10 +0530

    initial project

---
+++ MovieBookingUI/MovieBookingUI/MovieBookingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingUIApp.swift
+//  MovieBookingUI
+//
+//  Created by Priya Shankar on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
